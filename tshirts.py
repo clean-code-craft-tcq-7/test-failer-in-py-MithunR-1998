2de59561f1f52991fb0f867f5e625bb1a2097140
MithunR-1998 <mithunsuarez@gmail.com>
2025-07-29 16:16:45 +0530

    Update tshirts.py
    Adding additional check to confirm size is within a valid range

--- tshirts.py
+++ tshirts.py
@@ -1,6 +1,8 @@
 
 def size(cms):
-    if cms < 38:
+    if cms < 30 or cms > 50:
+        return 'F'  #F = Fail
+    elif cms < 38:
         return 'S'
     elif cms < 42:
         return 'M'
@@ -12,4 +14,6 @@ assert(size(37) == 'S')
 assert(size(38) == 'M') #Might be S or M, to be discussed M
 assert(size(40) == 'M')
 assert(size(43) == 'L')
+assert(size(5) == 'F')
+assert(size(55) == 'F')
 print("All is well (maybe!)")
