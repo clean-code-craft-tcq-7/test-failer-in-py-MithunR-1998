93bbcd9b81c69328b5228db46afa3f52f638355e
MithunR-1998 <mithunsuarez@gmail.com>
2025-08-03 19:08:40 +0530

    color code check is introduced

--- misaligned.py
+++ misaligned.py
@@ -1,13 +1,18 @@
+def get_color_code(major_color, minor_color):
+    return (major_color*5 + minor_color + 1)
 
 def print_color_map():
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
+            print(f'{get_color_code(i,j)} | {major} | {minor}')
     return len(major_colors) * len(minor_colors)
 
 
 result = print_color_map()
-assert(result == 25)
-print("All is well (maybe!)")
+assert(get_color_code(0,1) == 2)
+assert(get_color_code(1,3) == 9)
+assert(get_color_code(2,4) == 15)
+assert(get_color_code(3,0) == 16)
+assert(get_color_code(4,2) == 23)
