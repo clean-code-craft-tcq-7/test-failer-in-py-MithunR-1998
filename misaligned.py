d8e36b7ebb93b65287720d29babd776fcb49be84
MithunR-1998 <mithunsuarez@gmail.com>
2025-08-03 20:12:47 +0530

    Assert for '|'

--- misaligned.py
+++ misaligned.py
@@ -20,17 +20,29 @@ def generate_color_map():
     return code_max, major_max, minor_max, color_map
 
 def print_color_map(code_max, major_max, minor_max, color_map):
+    gen_print_str = []
     for code,major,minor in color_map:
         code_padded = code.ljust(code_max)
         major_padded = major.ljust(major_max)
         minor_padded = minor.ljust(minor_max)
-        print (f'{code_padded} | {major_padded} | {minor_padded}')
-
+        gen_print_str.append (code_padded +' | ' + major_padded + ' | ' +minor_padded)
+    for line in gen_print_str:
+        print (f'{line}')
+    return (gen_print_str)
 (c_max, maj_max, min_max, gen_map) = generate_color_map()
-print_color_map(c_max, maj_max, min_max, gen_map)
-#result = print_color_map()
+generated_str = print_color_map(c_max, maj_max, min_max, gen_map)
+
+#Asserts for color code
 assert(get_color_code(0,1) == 2)
 assert(get_color_code(1,3) == 9)
 assert(get_color_code(2,4) == 15)
 assert(get_color_code(3,0) == 16)
 assert(get_color_code(4,2) == 23)
+
+#Asserts for '|'
+#Always string position 3 an 12 are |
+assert(generated_str[0][3] == '|')
+assert(generated_str[7][3] == '|')
+assert(generated_str[13][12] == '|')
+assert(generated_str[24][3] == '|')
+assert(generated_str[17][12] == '|')
